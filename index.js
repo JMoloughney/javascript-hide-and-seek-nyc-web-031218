7d58c2932e814523f3f2ea07b6fbcce626733cad
JMoloughney <github email address>
2018-04-26 11:25:01 -0400

    Done.

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ function nestedTarget() {
 }
 
 function deepestChild(){
-let node = document.getElementById('grand-node')
-let next = node.children[0]
-while (next){
-  node = next
-  next = node.children[0]
+  let node = document.getElementById('grand-node')
+  let next = node.children[0]
+  while (next){
+    node = next
+    next = node.children[0]
   }
   return node
 }
+
+//first var //original top div
+//second var//the current child of our firstVar setting equal to the first child by [0]
+//drop down into while loop//
+//while(next){ while the the div has a child again remember [0] ^ keep running this block
+//node = next <-- setting up for the next run of this while loop to be the div that next(the child) refers to
+//next = node.children[0] <----setting up for the next run of while loop need to define next
+//                              for next run of while loop because it doesnt know
+//                             that next is now in the newly nested div scope we just got into
+//}
+//return node  <----once it breaks out of while/ your node will now have the value of the very last child
+
+
+
 
 function increaseRankBy(n){
   const rankedLists = document.querySelectorAll('.ranked-list')
