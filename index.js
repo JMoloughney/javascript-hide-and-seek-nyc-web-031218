45992b1a62ed662d4589cdff528df2be5d812a02
JMoloughney <github email address>
2018-04-26 10:15:03 -0400

    Done.

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ let next = node.children[0]
 while (next){
   node = next
   next = node.children[0]
-}
-return node
+  }
+  return node
 }
 
 function increaseRankBy(n){
   const rankedLists = document.querySelectorAll('.ranked-list')
-  for (let i = 0, l = rankedLists.length; i < l; i++){
+  for (let i = 0; i < rankedLists.length; i++){
     let children = rankedLists[i].children
 
-    for(let j= 0, k = children.length; j < k; j++){
+    for(let j= 0; j < children.length; j++){
       children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
   }
